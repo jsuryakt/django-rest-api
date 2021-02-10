74597b81223dc660f3273f7b4af59b4ab5c71973
Jayasurya <jsuryakt@gmail.com>
2021-02-11 00:27:26 +0530

    Changed name of URLField

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Api(models.Model):
     title = models.CharField(max_length = 300)
-    url = models.URLField(max_length = 1000)
+    image_url = models.URLField(max_length = 1000)
     content = models.TextField()
     date = models.DateField()
 
